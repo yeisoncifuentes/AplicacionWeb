1ecbf173f05e92355062decab0d113398a588858
YEISON CIFUENTES <yeisoncifuentes2@gmail.com>
2020-04-24 11:55:04 -0500

    creacion proyecto

---
+++ FormularioWeb/src/main/java/edu/unicundi/formularioweb/Index.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unicundi.formularioweb;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author YEISON
+ */
+@Named(value = "index")
+@RequestScoped
+public class Index {
+
+    /**
+     * Creates a new instance of Index
+     */
+    public Index() {
+    }
+    
+}
