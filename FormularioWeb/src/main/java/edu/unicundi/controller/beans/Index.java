37c302807742718db64391d8bd2d08e055da04d5
Camilo Andrés <cass465andres@gmail.com>
2020-04-28 15:14:22 -0500

    Capturar datos en Bean Index

---
+++ FormularioWeb/src/main/java/edu/unicundi/controller/beans/Index.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unicundi.controller.beans;
+
+import edu.unicundi.model.Empleado;
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author YEISON
+ */
+@Named(value = "index")
+@ViewScoped
+public class Index implements Serializable {
+
+    private Empleado empleado;
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String fechaNacimiento;
+    private int diasTrabajados;
+    private int sueldoBase;
+    private String educacion;
+    private String[] idiomas;
+
+    /**
+     * Creates a new instance of Index
+     */
+    public Index() {
+        System.out.println("Estoy en el constructor");
+    }
+
+    public void capturarDatos() {
+        if (educacion == null) {
+            educacion = "Ningun rango de educación";
+        }
+        if (idiomas.length == 0) {
+            idiomas = new String[1];
+            idiomas[0] = "No registra idiomas";
+        }
+        empleado = new Empleado(nombre, apellido, cedula, fechaNacimiento, diasTrabajados, sueldoBase, educacion, idiomas, sueldoBase);
+        System.out.println("Nombre: " + empleado.getNombre());
+        System.out.println("Apellido: " + empleado.getApellido());
+        System.out.println("Cedula: " + empleado.getCedula());
+        System.out.println("Fecha Nacimiento: " + empleado.getFechaNacimiento());
+        System.out.println("Dias Trabajados: " + empleado.getDiasTrabajados());
+        System.out.println("Sueldo Base: " + empleado.getSueldoBase());
+        System.out.println("Educación: " + empleado.getEducacion());
+        for (int i = 0; i < empleado.getIdiomas().length; i++) {
+            System.out.println("Idioma" + (i + 1) + ": " + empleado.getIdiomas()[i]);
+        }
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getDiasTrabajados() {
+        return diasTrabajados;
+    }
+
+    public void setDiasTrabajados(int diasTrabajados) {
+        this.diasTrabajados = diasTrabajados;
+    }
+
+    public int getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(int sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public String getEducacion() {
+        return educacion;
+    }
+
+    public void setEducacion(String educacion) {
+        this.educacion = educacion;
+    }
+
+    public String[] getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(String[] idiomas) {
+        this.idiomas = idiomas;
+    }
+
+}
---
+++ FormularioWeb/src/main/java/edu/unicundi/model/Empleado.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unicundi.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author cass465
+ */
+public class Empleado implements Serializable{
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String fechaNacimiento;
+    private int diasTrabajados;
+    private int sueldoBase;
+    private String educacion;
+    private String[] idiomas;
+    private int sueldoTotal;
+
+    public Empleado(String nombre, String apellido, String cedula, String fechaNacimiento, int diasTrabajados, int sueldoBase, String educacion, String[] idiomas, int sueldoTotal) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.fechaNacimiento = fechaNacimiento;
+        this.diasTrabajados = diasTrabajados;
+        this.sueldoBase = sueldoBase;
+        this.educacion = educacion;
+        this.idiomas = idiomas;
+        this.sueldoTotal = sueldoTotal;
+    }
+
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public int getDiasTrabajados() {
+        return diasTrabajados;
+    }
+
+    public int getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public String getEducacion() {
+        return educacion;
+    }
+
+    public String[] getIdiomas() {
+        return idiomas;
+    }
+
+    public int getSueldoTotal() {
+        return sueldoTotal;
+    }
+    
+}
