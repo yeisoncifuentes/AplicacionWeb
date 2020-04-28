becd2c36d0e409cf3920b5e1380e104a92e8cc23
Camilo Andrés <cass465andres@gmail.com>
2020-04-27 19:08:07 -0500

    Prueba con el nombre

--- FormularioWeb/src/main/java/edu/unicundi/formularioweb/Index.java
+++ FormularioWeb/src/main/java/edu/unicundi/formularioweb/Index.java
@@ -5,21 +5,29 @@
  */
 package edu.unicundi.formularioweb;
 
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
 
 /**
  *
  * @author YEISON
  */
 @Named(value = "index")
-@RequestScoped
+@ViewScoped
 public class Index {
-
+    private String nombre;
     /**
      * Creates a new instance of Index
      */
     public Index() {
+        System.out.println("estoy en el constructor");
     }
     
+    public void capturarDatos(){
+        System.out.println(nombre);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
