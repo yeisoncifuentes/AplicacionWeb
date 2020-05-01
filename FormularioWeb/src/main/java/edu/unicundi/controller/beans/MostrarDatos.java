22edfaeee027575019eeff8385e067d113193dfa
Camilo Andrés <cass465andres@gmail.com>
2020-04-30 23:09:57 -0500

    Creacion de segunda vista con su bean

---
+++ FormularioWeb/src/main/java/edu/unicundi/controller/beans/MostrarDatos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.unicundi.controller.beans;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author cass465
+ */
+@Named(value = "mostrarDatos")
+@ViewScoped
+public class MostrarDatos {
+
+    /**
+     * Creates a new instance of MostrarDatos
+     */
+    public MostrarDatos() {
+    }
+    
+}
